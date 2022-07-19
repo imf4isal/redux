ab35011c7b5bf0c241dd63c9224e93111db3ab0c
Faisal <md.faisal1899@yahoo.com>
2022-07-19 15:30:31 +0600

    added multiple state via redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,24 +4,29 @@ import classes from './Counter.module.css';
 const Counter = () => {
 
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
+
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
+
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
+  };
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'});
+    dispatch({type: 'increment', value: 5});
   };
+  
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch({type: 'decrement', value: 2});
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
-
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
