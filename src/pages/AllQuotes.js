10b8fde8deb679ee873728df3d8929ff62b30c1d
Faisal <md.faisal1899@yahoo.com>
2022-07-24 09:38:44 +0600

    implemented not found, programatic navigation

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,12 @@
-const AllQuotes = () => {
+import QuoteList from '../components/quotes/QuoteList';
 
-    return <h1>All Quotes Page</h1>
-}
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
+const AllQuotes = () => {
+  return <QuoteList quotes={DUMMY_QUOTES} />
+};
 
 export default AllQuotes;
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,6 +1,14 @@
+import { useHistory } from 'react-router-dom';
+import QuoteForm from '../components/quotes/QuoteForm';
 const NewQuote = () => {
 
-    return <h1>New Quote</h1>
+    const history = useHistory();
+    const addQuoteHandler = (quotedata) => {
+        console.log(quotedata)
+        history.push('/quotes');
+    }
+
+    return <QuoteForm onAddQuote = {addQuoteHandler} />
 }
 
 export default NewQuote;
