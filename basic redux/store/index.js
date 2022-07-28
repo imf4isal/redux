cfa29a49201fe4038a0f27467bf99dc7adc990f2
Faisal <md.faisal1899@yahoo.com>
2022-07-28 10:33:25 +0600

    completed develpment spa..ready for building.

--- basic redux/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth';
-import counterReducer from './count';
-
-const store = configureStore({
-  reducer: { counter: counterReducer, auth: authReducer }
-});
-
-export default store;
